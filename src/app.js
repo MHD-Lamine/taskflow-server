1f1e99120dbcdd0732309ab8a3a41914f042c5cf
Diabate Mohamed Lamine <mohaldiabate@gmail.com>
2026-05-17 02:05:28 +0000

    feat: implement JWT authentication system

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+const protect = require("./middlewares/authMiddleware");
+
 const app = express();
 
 app.use(cors());
@@ -12,5 +15,10 @@ app.get("/", (req, res) => {
     message: "TaskFlow API running",
   });
 });
+app.get("/api/profile", protect, (req, res) => {
+  res.json(req.user);
+});
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
